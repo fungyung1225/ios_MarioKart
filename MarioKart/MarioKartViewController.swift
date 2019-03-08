b48df20dc27c36af5f063624453d395eb36c7ad0
Fung Yung <fungyung1225@gmamil.com>
2019-03-08 17:52:14 -0500

    add images add bg, 5 karts to view add 5 Pan Gesture Recognizer to each kart link gesture to each kart add new uiviewcontroller add new action for Kart

---
+++ MarioKart/MarioKartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MarioKartViewController.swift
+//  MarioKart
+//
+//  Created by Fung on 3/8/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class MarioKartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
